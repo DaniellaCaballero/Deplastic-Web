d16b3f0563c13da8043d6258c652259412e43967
Daniella <a22dancabgom@inspedralbes.cat>
2022-10-24 12:39:56 +0200

    Login Commit 1

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3001;
+
+app.listen(PORT, () =>{
+    console.log("Servidor ences mofo");
+});
+
+const cors = require("cors");
+
